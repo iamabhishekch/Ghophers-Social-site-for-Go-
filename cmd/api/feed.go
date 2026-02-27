6214216907dca9a69c8768dc70c0ad756886e91c
Abhishek <chaurasiyaa750@gmail.com>
2026-02-27 15:56:25 +0530

    complete pagination and filters

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -1,12 +1,38 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/iamabhishekch/Social/internal/store"
+)
 
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
 
+	// pagination, filter, sort
+
+	// giving default values to pagination properperties
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+	}
+
+	// parsing values send by user
+	fq, err := fq.Parse(r)
+	if err != nil{
+		app.badRequestResponse(w,r,err)
+		return
+	}
+
+	// validating recevied values from user
+	if err := Validate.Struct(fq); err != nil{
+		app.badRequestResponse(w,r,err)
+		return
+	}
+
 	ctx := r.Context()
 
-	feed, err := app.store.Posts.GetUserFeed(ctx, 23)
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(23), fq)
 	if err != nil {
 		app.internalServerError(w, r, err)
 	}
